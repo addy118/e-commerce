c09c0534651a90e660de59341ac450e6b105c79a
Aditya Kirti <addyyy118@gmail.com>
2024-12-13 12:46:49 +0530

    Initial Setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,7 @@ import "./App.css";
 import { Globe } from "lucide-react";
 
 function App() {
-  return (
-    <div className="flex items-center justify-center gap-2">
-      <Button
-        sx={{
-          px: 4,
-          fontSize: 48,
-          "&:focus": {
-            outline: "none",
-          },
-        }}
-        startIcon={<Globe color="#1976d2" size={48} />}
-        variant="outlined"
-      >
-        Hello world
-      </Button>
-    </div>
-  );
+  return 
 }
 
 export default App;
